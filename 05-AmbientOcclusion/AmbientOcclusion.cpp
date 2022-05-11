61b31304b6c53e7b727c86529c32dc511389fba5
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-05-11 10:11:07 -0700

    Add AmbientOcclusion rasterized G-Buffer pass

---
+++ 05-AmbientOcclusion/AmbientOcclusion.cpp
@@ -0,0 +1,17 @@
+#include "../SharedUtils/RenderingPipeline.h"
+#include "../03-RasterGBuffer/Passes/SimpleGBufferPass.h"
+#include "../03-RasterGBuffer/Passes/CopyToOutputPass.h"
+#include "Passes/AmbientOcclusionPass.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+	RenderingPipeline pipeline;
+
+	pipeline.setPass(0, SimpleGBufferPass::create());
+	pipeline.setPass(1, CopyToOutputPass::create());
+
+	SampleConfig config;
+	config.windowDesc.fullScreen = false;
+	config.windowDesc.title = "Hybrid rasterized and ray traced ambient occlusion";
+
+	RenderingPipeline::run(&pipeline, config);
+}
