9daa5e35c801365a11e871e3032fc99c34531a6e
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-05-12 16:41:40 -0700

    Add TemporalAccumulationPass:execute

--- 06-TemporalAccumulation/Passes/TemporalAccumulationPass.cpp
+++ 06-TemporalAccumulation/Passes/TemporalAccumulationPass.cpp
@@ -33,3 +33,44 @@ void TemporalAccumulationPass::initScene(RenderContext *pRenderContext, Falcor::
         mpLastCameraMatrix = mpScene->getActiveCamera()->getViewMatrix();
     } 
 }
+
+void TemporalAccumulationPass::execute(RenderContext *pRenderContext) {
+    // The texture where the accumulation is done.
+    Falcor::Texture::SharedPtr accumTexture = mpResManager->getTexture(mAccumChannel);
+
+    // mDoAccumulation is set through the GUI to enable/disable temporal accumulation.
+    if (!accumTexture || !mDoAccumulation) {
+        return;
+    }
+
+    if (hasCameraMoved()) {
+        // Reset state and start accumulation over.
+        mNumFramesAccum = 0;
+        // When hasCameraMoved() returns true, the scene and the active camera are 
+        // guaranteed to exist.
+        mpLastCameraMatrix = mpScene->getActiveCamera()->getViewMatrix();
+    }
+
+    // Execute the pixel shader, passing it down the last frame and accumulation texture.
+    // The pixel shader will do a weighted combination of the last frame and the
+    // accumulation texture to obtain an average.
+    auto pixelShaderVars = mpAccumShader->getVars();
+    pixelShaderVars["PerFrameCB"]["mNumFramesAccum"] = mNumFramesAccum++;
+    pixelShaderVars["gLastFrame"] = mpLastFrame;
+    pixelShaderVars["gCurFrame"] = accumTexture;
+    mpAccumShader->execute(pRenderContext, mpGfxState);
+
+    // blit copies a source SRV into a destination RTV.
+    pRenderContext->blit(mpInternalFbo->getColorTexture(0)->getSRV(), accumTexture->getRTV());
+    // Save the rendered frame to pass it down to the next frame's pixel shader.
+    pRenderContext->blit(mpInternalFbo->getColorTexture(0)->getSRV(), mpLastFrame->getRTV());
+}
+
+bool TemporalAccumulationPass::hasCameraMoved() {
+    // The camera has moved when the current view matrix is different from the previously
+    // saved view matrix. The view matrix is saved right after the camera moves and when
+    // a new scene is loaded.
+    return mpScene
+        && mpScene->getActiveCamera()
+        && (mpLastCameraMatrix != mpScene->getActiveCamera()->getViewMatrix());
+}
--- 06-TemporalAccumulation/Passes/TemporalAccumulationPass.h
+++ 06-TemporalAccumulation/Passes/TemporalAccumulationPass.h
@@ -2,21 +2,36 @@
 #include "../SharedUtils/FullscreenLaunch.h"
 #include "../SharedUtils/RenderPass.h"
 
+// Temporal accumulation of frames takes place as long as the camera doesn't move or the
+// scene changes.
 class TemporalAccumulationPass : public ::RenderPass, inherit_shared_from_this<::RenderPass, TemporalAccumulationPass> {
 protected:
+    // The ResourceManager refers to textures by channel name;
     std::string mAccumChannel;
 
+    // Number of frames that have been accumulated so far. It gets restarted every time the
+    // camera moves or when a new scene is loaded.
     uint32_t mNumFramesAccum;
     
     Falcor::Scene::SharedPtr mpScene;
     // Cameras belong to scenes; every scene defines its own cameras, one of which is active at 
-    // any one moment. When loading a new scene, we need to adopt the scene's active camera.
+    // any one moment. When loading a new scene or when the camera has moved, we need to save the
+    // active camera's view matrix because we use it to detect when the accumulation texture is 
+    // no longer valid and we need to start it over.
     glm::mat4 mpLastCameraMatrix;
 
     // State for rasterization pipeline.
     Falcor::GraphicsState::SharedPtr mpGfxState;
 
+    // FBO, or Frame Buffer Object, is OpenGL terminology for the collection of color and
+    // depth/stencil buffers.
+    Falcor::Fbo::SharedPtr mpInternalFbo;
+
     FullscreenLaunch::SharedPtr mpAccumShader;
+
+    Falcor::Texture::SharedPtr mpLastFrame;
+
+    bool mDoAccumulation;
 
     TemporalAccumulationPass(const std::string &accumulationBuffer) : RenderPass("Accumulation Pass", "Accumulation Pass Options") {
         mAccumChannel = accumulationBuffer;
@@ -26,6 +41,11 @@ protected:
 
     // Called when loading a new scene. Some state needs to be reset
     void initScene(RenderContext *pRenderContext, Falcor::Scene::SharedPtr pScene) override;
+
+    void execute(RenderContext *pRenderContext) override;
+
+    // Temporal accumulation starts over when the camera moves. 
+    bool hasCameraMoved();
 
 public:
     using SharedPtr = std::shared_ptr<TemporalAccumulationPass>;
